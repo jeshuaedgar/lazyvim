500f63286bd6732559fcb8e76f292b37cdb00c30
JeshuaEdgar <jeshuaedgar@icloud.com>
2025-06-30 10:30:53 +0200

    added: oil lsp

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -74,6 +74,16 @@ return {
       vim.keymap.set("n", "<leader>e", function(dir)
         require("oil").open(dir)
       end, { desc = "Open file [e]xplorer" })
+
+      -- rename lsp actions
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "OilActionsPost",
+        callback = function(event)
+          if event.data.actions.type == "move" then
+            Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+          end
+        end,
+      })
     end,
   },
   {
