66cc747d47ad9f804eb909b881023e1da6c8235d
JeshuaEdgar <jeshuaedgar@icloud.com>
2025-08-28 14:32:04 +0200

    moved: oil to extras for fast file explorer switching

---
+++ lua/plugins/extras/util/fyler.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    disabled = true,
+    priority = 101,
+  },
+  {
+    "A7Lavinraj/fyler.nvim",
+    dependencies = { "echasnovski/mini.icons" },
+    branch = "stable",
+    opts = {},
+  },
+}
---
+++ lua/plugins/extras/util/oil.lua
@@ -0,0 +1,86 @@
+return {
+  "stevearc/oil.nvim",
+  priority = 100,
+  dependencies = {
+    { "echasnovski/mini.icons", opts = {} },
+    { "refractalize/oil-git-status.nvim" },
+  },
+  ---@module 'oil'
+  ---@type oil.SetupOpts
+  opts = {
+    default_file_explorer = true,
+    columns = {
+      "mtime",
+      "icon",
+    },
+    win_options = {
+      signcolumn = "yes:2",
+      winbar = "%!v:lua.get_oil_winbar()",
+      conceallevel = 3,
+    },
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+    lsp_file_methods = {
+      enabled = true,
+      autosave_changes = true,
+    },
+    constrain_cursor = "editable",
+    watch_for_changes = true,
+    view_options = {
+      show_hidden = true,
+      is_always_hidden = function(name, _)
+        local ban_list = { ".DS_Store", ".git", ".." }
+        return vim.list_contains(ban_list, name)
+      end,
+      is_hidden_file = function(name, _)
+        local ban_list = { "__pycache__" }
+        return vim.list_contains(ban_list, name)
+      end,
+    },
+    keymaps = {
+      ["H"] = {
+        desc = "Toggle hidden files",
+        callback = function()
+          require("oil").toggle_hidden()
+        end,
+      },
+      ["<leader>e"] = {
+        desc = "Close oil",
+        callback = function()
+          require("oil").close()
+        end,
+      },
+    },
+  },
+  config = function(_, opts)
+    -- place folder in winbar
+    function _G.get_oil_winbar()
+      local dir = require("oil").get_current_dir()
+      if dir then
+        return vim.fn.fnamemodify(dir, ":~")
+      else
+        return vim.api.nvim_buf_get_name(0)
+      end
+    end
+
+    require("oil").setup(opts)
+
+    require("oil-git-status").setup({ show_ignored = true })
+
+    -- git integration
+    -- set keymap
+    vim.keymap.set("n", "<leader>e", function(dir)
+      require("oil").open(dir)
+    end, { desc = "Open file [e]xplorer" })
+
+    -- rename lsp actions
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "OilActionsPost",
+      callback = function(event)
+        if event.data.actions.type == "move" then
+          Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+        end
+      end,
+    })
+  end,
+}
