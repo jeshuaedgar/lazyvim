01fc53abb41176fc5db44565834bb170e1bc7efa
JeshuaEdgar <jeshuaedgar@icloud.com>
2025-10-27 10:14:27 +0100

    added: fyler

--- lua/plugins/extras/util/fyler.lua
+++ lua/plugins/extras/util/fyler.lua
@@ -6,8 +6,15 @@ return {
   },
   {
     "A7Lavinraj/fyler.nvim",
-    dependencies = { "echasnovski/mini.icons" },
+    dependencies = { "nvim-mini/mini.icons" },
     branch = "stable",
-    opts = {},
+    opts = {
+      close_on_select = true,
+      confirm_simple = true,
+      default_explorer = true,
+    },
+    config = function(_, opts)
+      require("fyler").setup(opts)
+    end,
   },
 }
